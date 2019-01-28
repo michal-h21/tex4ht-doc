e68d4490839f3353819e5e394103815e63747415
Michal Hoftich <michal.h21@gmail.com>
2019-01-28 11:05:18 +0100

    Use joined colors in the html

--- filters/join_colors.lua
+++ filters/join_colors.lua
@@ -9,7 +9,7 @@ local function extract_colors(csscontent)
   -- delete the color ids and save the used colors
   csscontent = csscontent:gsub("%#(textcolor.-)%s*{%s*color%s*%:%s*(.-)%s*%}%s", function(id, color)
     -- convert rgb() function to hex value and generate the span name
-    local converted = ".textcolor-" .. color:gsub("rgb%((.-),(.-),(.-)%)", function(r,g,b)
+    local converted = "textcolor-" .. color:gsub("rgb%((.-),(.-),(.-)%)", function(r,g,b)
       return string.format("%02x%02x%02x", tonumber(r), tonumber(g), tonumber(b))
     end)
     -- save the id and used color
@@ -20,7 +20,7 @@ local function extract_colors(csscontent)
   -- add the used colors to css
   local t = {}
   for class, color in pairs(used_colors) do
-    t[#t+1] = string.format("%s{color:%s;}", class, color)
+    t[#t+1] = string.format(".%s{color:%s;}", class, color)
   end
   return csscontent .. table.concat(t, "\n")
 end
@@ -54,6 +54,18 @@ end
 local function join_colors(dom)
   -- find css files in the current HTML file and join the colors
   process_css_files(dom)
+  for _, span in ipairs(dom:query_selector("span")) do
+    local id = span:get_attribute("id")
+    if id then
+      -- test if the id is in the saved colors
+      local class = colors[id]
+      if class then
+        -- remove the id
+        span:set_attribute("id", nil)
+        span:set_attribute("class", class)
+      end
+    end
+  end
 
   return dom
 end
